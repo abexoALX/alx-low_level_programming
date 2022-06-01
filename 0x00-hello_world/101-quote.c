08a1a5b584690f597873c092d7ab2b582f26cef0
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-01 13:35:31 -0700

    write functionsd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch) -1);
-	return(1);
+
+	write(2, ch, sizeof(ch));
+
+	return (1);
 }
