0432b99e47f1ddad1ad19f1144133b666b866b5e
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-01 12:09:57 -0700

    ass

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
  *
- *  * main - Entry point
+ *  * main - main block
  *
- *   *
+ *   * Return: 0
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *    */
 int main(void)
+
 {
-	  puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 
 }
