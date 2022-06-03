10e3d25b1686d017301d0530cc2439cd6eabe1d3
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-03 09:28:42 -0700

    sxsdd

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,11 +25,11 @@ while (c < 10)
 if (c != d && d != e && e < d && d < c)
 {
 
-	putchar( '0' +  e );
+	putchar('0' +  e);
 
-	putchar( '0' +  d );
+	putchar('0' +  d);
 
-	putchar( '0' +  c );
+	putchar('0' +  c);
 
 if (c + d + e != 9 + 8 + 7)
 {
