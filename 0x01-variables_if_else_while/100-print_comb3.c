fe86be798fd360ff4a99ae74d9c1aa90e13139c8
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-03 09:52:32 -0700

    sxsddsas

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,12 +3,6 @@
 /** 
 *main - main block
 *Description: Write a program that prints all possible
-*different combinations of two digits
-*Numbers must be separated by ,, followed by a space
-*The two digits must be different
-*01 and 10 are considered the same combination of the two digits 0 and 1
-*Print only the smallest combination of two digits
-*Numbers should be printed in ascending order, with two digits
 *Return: 0
 */
 int main(void)
@@ -25,22 +19,20 @@ while (c < 10)
 
 if (d != c && d < c)
 {
-putchar('0' + d);
-putchar('0' + c);
+	putchar('0' + d);
+
+	putchar('0' + c);
 }		
 
 if (c + d != 17)															{
-putchar(',');																putchar(' ');
+	putchar(',');
+
+	putchar(' ');
 }
 c++;
 }
-
-
 d++;
-
 }
 putchar('\n');
-
 return (0);
-
 }
