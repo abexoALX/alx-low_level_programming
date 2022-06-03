f4daba958e6ec3180ffe3660999e699b56502ada
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-03 10:26:21 -0700

    asaw

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -23,18 +23,18 @@ while (d < 10)
 c = 0;
 while (c < 10)
 {
-if (!(f == c && e == d))
-{
-	putchar('0' + f);
-	putchar('0' + e);
-	putchar(' ');
-	putchar('0' + d);
-	putchar('0' + c);
-																		if (!(f + e == 18 && c + d == 17 && d == 9))	
+	if (!(f == c && e == d))
 	{
-		putchar(',');																putchar(' ');															}	
-}
-c++;
+		putchar('0' + f);
+		putchar('0' + e);
+		putchar(' ');
+		putchar('0' + d);	
+		putchar('0' + c);
+		if (!(f + e == 18 && c + d == 17 && d == 9))	
+		{
+			putchar(',');																putchar(' ');															}	
+	}
+	c++;
 }
 d++;
 }
