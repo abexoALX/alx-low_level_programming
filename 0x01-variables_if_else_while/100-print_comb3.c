bf48b4d328b5a9495e937da0dcc2e723853d61de
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-03 10:10:05 -0700

    sxsddsasfss

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,10 +21,11 @@ if (d != c && d < c)
 
 	putchar('0' + c);
 
-	if (c + d != 17)															{
-	putchar(',');
+	if (c + d != 17)
+	{
+		putchar(',');
 
-	putchar(' ');
+		putchar(' ');
 	}
 }
 c++;
