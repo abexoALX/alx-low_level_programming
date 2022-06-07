dfc7b22264dc45932bc3c9d48c81c9800b25ef3e
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-07 04:42:34 -0700

    ews

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_last_digit - return last digit
+*@n : number to check
+*Return:0 or 1
+*/
+int print_last_digit(int n)
+{
+n = n >= 0 ? n : n * -1;
+int digit = n % 10;
+_putchar('0' + digit);
+return (digit);
+}
