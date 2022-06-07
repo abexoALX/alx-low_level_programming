0a4496b60b4bc9207300c0ee8e4d4a94c4197ae9
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-07 14:17:46 -0700

    as

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - Entry point
+*Return:0
+*/
+int main(void)
+{
+char c[8] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+	_putchar(c[i]);
+	i++;
+}
+_putchar('\n');
+return (0);
+}
