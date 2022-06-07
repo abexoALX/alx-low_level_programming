a0b29a7bdc583e3292e7c6fa1c82481ae7b22e11
abenixo <abenezer.ashenafi.alx@gmail.com>
2022-06-07 03:19:16 -0700

    asds

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet 10 times
+*Return:void
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+while (i < 10)
+{
+	print_alphabet();
+
+}
+_putchar('\n');
+
+}
