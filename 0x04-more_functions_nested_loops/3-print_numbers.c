709c2a4d17e064d402127f5f05ef9326e2511b71
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-06-09 12:56:32 +0300

    saee

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers - prints numbers
+*Return:0
+*/
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		_putchar(n);
+		n++;
+
+	}
+	_putchar('\n');
+
+}
