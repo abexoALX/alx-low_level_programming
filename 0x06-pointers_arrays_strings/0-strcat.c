70f718c3925d8a47bac469ba7a687b7c221890c5
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-06-16 12:09:47 +0300

    asrea

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * *_strcat - concatenates two strings
- * @dest: pointer destination
- * @src: pointer source
- * Return: void
+**_strcat - concatenates two strings
+*@dest: pointer destination
+*@src: pointer source
+*Return: void
 */
 char *_strcat(char *dest, char *src)
 {
