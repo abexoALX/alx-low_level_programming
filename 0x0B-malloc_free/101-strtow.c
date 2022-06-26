25e8348ec3d21d4b2b4e32b605fc37e07caa3771
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-06-26 23:55:06 +0300

    asdsdsdsafrdsderrrffrwdd

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -61,12 +61,10 @@ unsigned int nbr_spaces(char *s)
 char **strtow(char *str)
 {
 int i;
-int spaces = nbr_spaces(str);
 char **tokens = NULL;
 char *token;
 int checkingSpace = 0;
 int word = 0;
-
 if (!tokens)
 {
 	printf("Failed");
