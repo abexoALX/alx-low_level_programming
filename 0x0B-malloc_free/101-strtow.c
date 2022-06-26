cb9f1033c1c2d4ac067c2a39e5455a95c3a516c5
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-06-26 23:53:15 +0300

    asdsdsdsafrdsderrrffrwd

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
- */
-
-
+*_strlen - find length of a string
+*@s: string
+*Return: int
+*/
 int _strlen(char *s)
 {
 int size = 0;
@@ -13,14 +11,12 @@ for (; s[size] != '\0'; size++)
 ;
 return (size);
 }
-
 /**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
-
+**str_concat - concatenates two strings
+*@s1: string 1
+*@s2: string 2
+*Return: pointer
+*/
 char *str_addChar (char *str, char c)
 {
 int size, i;
@@ -40,14 +36,11 @@ m[i + 2] = '\0';
 
 return (m);
 }
-
-
 /**
- * *nbr_spaces - return the number of occurent of a string
- * @s: string to check
- * Return: int
- */
-
+**nbr_spaces - return the number of occurent of a string
+*@s: string to check
+*Return: int
+*/
 unsigned int nbr_spaces(char *s)
 {
 	int i, cmpt = 0;
@@ -60,18 +53,16 @@ unsigned int nbr_spaces(char *s)
 
 	return (cmpt + 1);
 }
-
-
 /**
-  *strtow - split a sentence into multiple words.
-  *@str: the string passed as argument.
-  *Return: tokens
-  */
+*strtow - split a sentence into multiple words.
+*@str: the string passed as argument.
+*Return: tokens
+*/
 char **strtow(char *str)
 {
 int i;
 int spaces = nbr_spaces(str);
-char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+char **tokens = NULL;
 char *token;
 int checkingSpace = 0;
 int word = 0;
