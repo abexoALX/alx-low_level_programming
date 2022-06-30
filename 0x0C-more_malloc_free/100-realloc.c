2bbcd0e90f72cc14bc6dc799e432106ddf574751
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-06-30 14:26:49 +0300

    asdefewiaa

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,52 +1,32 @@
 #include "main.h"
 /**
-*_memcpy - copy memory data from src to dest
-*@dest: memory destination
-*@src: memory source
-*@n: size of new memory
-*/
-void _memcpy(void *dest, void *src, size_t n)
-{
-	size_t i;
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
-
-	for (i = 0; i < n; i++)
-		cdest[i] = csrc[i];
-}
-/**
-**_realloc - reallocates a memory block using malloc and free
-*@ptr: array length
-*@old_size: size of old memory
-*@new_size: size of new memory
-*Return: pointer to new memory
+*_realloc -  reallocates a memory block using malloc and free
+*@ptr: pointer
+*@old_size: old size
+*@new_size: new size
+*Return: pointer
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-void *newPtr;
-if (new_size == 0)
-{
-	if (ptr != NULL)
-		free(ptr);
-	return (NULL);
-}
-else if (!ptr)
-{
-	return (malloc(new_size));
-}
-else if (new_size <= old_size)
-{
-	return (ptr);
-}
-else
-{
-	newPtr = malloc(new_size);
-	if (newPtr)
-	{
-		_memcpy(newPtr, ptr, old_size);
-		free(ptr);
-	}
-	return (newPtr);
-}
-return (0);
+ char *clone, *relloc;
+ unsigned int i;
+
+ if (ptr != NULL)
+ clone = ptr;
+ else
+ { return (malloc(new_size)); }
+ if (new_size == old_size)
+ return (ptr);
+ if (new_size == 0 && ptr != NULL)
+ { free(ptr);
+ return (0); }
+ relloc = malloc(new_size);
+ if (relloc == NULL)
+ return (0);
+ for (i = 0; i < (old_size || i < new_size); i++)
+ {
+  *(relloc + i) = clone[i];
+ }
+ free(ptr);
+return (relloc);
 }
