b871e0216af3ab0cae7c1dbd7a2fac62ddbd8c48
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-07-14 14:30:33 +0300

    asdasda

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * premain - run before main
- */
+*premain - run before main
+*/
 void __attribute__ ((constructor)) premain()
 {
 printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
