afce4d0e6667b417a9658c77d7964da854887a94
abexoALX <abenezer.ashenafi.alx@gmail.com>
2022-09-23 20:47:45 +0300

    ak

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,43 +1,25 @@
 #include "hash_tables.h"
 /**
- * hash_table_delete - deletes a hash table
- * @ht: the hash table you want to delete
- */
+*hash_table_delete - prints all hash values with key
+*@ht: struct poniter
+*Return: void
+*/
 void hash_table_delete(hash_table_t *ht)
 {
 unsigned long int i;
-hash_node_t *node;
-
-if (ht == NULL)
-	return;
-
-for (i = 0; i < ht->size; i++)
+hash_node_t *temp, *hold;
+for (i = 0 ; i < ht->size; i++)
 {
-	node = ht->array[i];
-	free_hash_list(node);
+temp = ht->array[i];
+while (temp != NULL)
+{
+hold = temp;
+temp = temp->next;
+free(hold->key);
+free(hold->value);
+free(hold);
+}
 }
 free(ht->array);
 free(ht);
-}
-
-
-/**
- * free_hash_list - frees a hash_node_t list
- * @head: head of linked list
- */
-void free_hash_list(hash_node_t *head)
-{
-	hash_node_t *current;
-	hash_node_t *next;
-
-	current = head;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		free(current->key);
-		free(current->value);
-		free(current);
-		current = next;
-	}
 }
